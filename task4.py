26ed9eb9bf9b904c2fe0b90a14772f9143c73826
Baiyzbek <kutmanov2005@gmail.com>
2024-10-31 10:29:55 +0600

    task4

---
+++ task4.py
@@ -0,0 +1,12 @@
+'What is the difference between switch and checkout? What does git merge do?'
+'git checkout for switching branches and restoring file states, git switch is only for switching branches'
+
+
+'What happens to the commit history after rebase and why?'
+'rebase re-writes projects history by creating new commits for each commit in the original branch, tyhe major benefit of rebasing is that you get a much cleaner project history'
+
+
+'How to understand which branch is remote and which is local in the terminal?'
+'git branch for local branches'
+'git branch -r for remote branches'
+'git branch -a for local and remote branches'
